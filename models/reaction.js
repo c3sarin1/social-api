2ad3f7434f6076c170de37c987d57b679802a818
Cesar Olguin <c3sarin3@gmail.com>
2022-03-11 13:19:36 -0500

    added files in modules

---
+++ models/reaction.js
@@ -0,0 +1,35 @@
+const { Schema, Types } = require('mongoose');
+const moment = require('moment');
+
+const reactionSchema = new Schema ({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId
+    },
+
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+
+    userName: {
+        type: String,
+        required: true
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAt) => moment(createdAt).format('LLL')
+    }
+},
+{
+    toJSON: {
+      getters: true,
+      virtuals: true
+    },
+    id: false,
+  })
+
+module.exports = reactionSchema
